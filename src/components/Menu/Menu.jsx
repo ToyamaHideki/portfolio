9606a689a79fd8ad43f2951defab14a6c076e964
ToyamaHideki <96602926+ToyamaHideki@users.noreply.github.com>
2022-09-11 10:51:26 +0900

    ハンバーガーメニューの作成 ある程度の挙動が完了

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./style/Menu.scss";
 
 const Menu = () => {
+
+  const [menuState, setMenuState] = useState(false);
+
+
   return (
     <>
-      <nav>
-       
+
+    <button className={`mobile_menu_icon ${menuState ? "open":"false"}`} onClick={() => {setMenuState(!menuState); console.log(menuState)}}>
+      <span></span>
+      <span></span>
+      <span></span>
+    </button>
+
+      <nav className={menuState ? "open" : "close"}>
+        <ul>
+          <li>menu</li>
+          <li>menu</li>
+          <li>menu</li>
+          <li>menu</li>
+        </ul>
       </nav>
 
     </>
